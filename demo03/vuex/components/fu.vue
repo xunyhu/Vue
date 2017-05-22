c8d4efcc90e3d1f492b41448dfe79c8061b53b14
HuRui <1194547767@qq.com>
2017-05-22 21:03:35 +0800

    vuex

---
+++ demo03/vuex/components/fu.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<header :logo="logoMsg"></header>
+	</div>
+</template>
+<script>
+	import zi from './zi.vue'
+	export default {
+		data() {
+			return {
+				logoMsg: "xunyihu"
+			}
+		},
+		components : {
+			zi
+		}
+	}
+</script>
